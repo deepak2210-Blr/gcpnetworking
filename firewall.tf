5841228da8f03a928fc374107146cf8263c1e4d0
deepak2210-Blr <deepak2210blr@gmail.com>
2024-12-15 05:59:09 +0000

    adding the firewall

---
+++ firewall.tf
@@ -0,0 +1,16 @@
+resource "google_compute_firewall" "firewall" {
+  name    = "my-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
